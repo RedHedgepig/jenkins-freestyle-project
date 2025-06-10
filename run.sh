4a470eaa7bee0cf4c310483a282b83ebff3c3193
RedHedgepig <alexcorrish@hotmail.com>
2025-06-10 10:36:15 +0100

    Updatetwo run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 echo 'hello from run.sh'
 echo 'this is being run from a Jenkins freestyle job'
-echo 'and it's proud of being run from a Jenkins freestyle job'
+echo "and it's proud of being run from a Jenkins freestyle job"
 echo 'and it hopes it can be run from a Jenkins freestyle job everyday'
